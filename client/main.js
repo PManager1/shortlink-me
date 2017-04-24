584a30651e98d2ae7b2265dac29f6aab94864b5a
jpca999 <jpca999@gmail.com>
2017-04-24 19:03:46 -0400

    -

--- client/main.js
+++ client/main.js
@@ -8,14 +8,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1> hello there !  </h1>
+        <h1> Shorten Link there !  </h1>
       </div>
     );
   }
 }
 
 Meteor.startup( () => {
-  ReactDOM.render(<App />, document.querySelector('.container'));
+  ReactDOM.render(<App />, document.querySelector('.render-target'));
 
 });
 
